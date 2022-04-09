1bfbf7ddbecbd187f0f6bb48632fa35247019ab6
Roberto Pedroza <Roberto.Pedroza@autozone.com>
2022-04-09 13:46:20 -0600

    New requests added to the postman collection

--- CustomerREST/src/main/java/com/assigment/TopicRepository.java
+++ CustomerREST/src/main/java/com/assigment/TopicRepository.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.assigment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- *
- * @author Antonio Pedroza
- */
+
 public interface TopicRepository extends JpaRepository<Topic, Integer>{
     
 }
